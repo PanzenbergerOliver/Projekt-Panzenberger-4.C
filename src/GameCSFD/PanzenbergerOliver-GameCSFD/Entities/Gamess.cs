a294ebd0aea0f26bc0fe745fbc8f80cdf8dd19fc
OliverPanzenberger <panzenbergeroliver@sssvt.cz>
2026-01-18 20:44:08 +0100

    2 týden

---
+++ src/GameCSFD/PanzenbergerOliver-GameCSFD/Entities/Gamess.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PanzenbergerOliver_GameCSFD.Entities
+{
+    [Table("gamess")]
+
+    public class Gamess
+    {
+        [Key]
+        [Column("id")]
+        public string Id { get; set; }
+        [Column("name")]
+        public double Name { get; set; }
+        public List<Game> Games { get; set; } = new List<Game>();
+    }
+}
